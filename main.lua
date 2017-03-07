cf9b32c0acda0dfbf6e3b4dabe214bacdc672468
MrSwayne <MrSwayne@users.noreply.github.com>
2017-03-07 10:54:35 +0000

    Delete main.lua

--- main.lua
+++
@@ -1,9 +0,0 @@
-local composer = require "composer"
-local g = require "globals"
-
-native.setProperty("windowMode", "fullscreen")
--- Hides the status bar
-display.setStatusBar( display.HiddenStatusBar )
-
---Loads the menu screen
-composer.gotoScene( "menu" )
