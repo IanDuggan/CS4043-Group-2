691d082848bc9466aaff2cc6ba4089ae0f092f81
IanDuggan <16151135@studentmail.ul.ie>
2017-03-07 11:28:50 +0000

    wasd movement added

--- levels/level1.lua
+++ levels/level1.lua
@@ -37,10 +37,10 @@ function scene:create( event )
 	--adds xap to the game
 	local xap = display.newImageRect( "capnXap.png", 90, 90 )
 	physics.addBody( xap, "dynamic", {friction = .5, bounce = 0})
-	xap.x = 50; xap.y = G.height - 100
+	xap.x = G.width / 2; xap.y = G.height - 100
 	
 	--Testing around with movement of Xap
-	
+	--[[
 	--add left joystick
 	local left = display.newRect(0,0,160,160)
 	left.x = 50; left.y = 280;
@@ -48,6 +48,10 @@ function scene:create( event )
 	--add right joystick
 	local right = display.newRect(0,0,160,160)
 	right.x = G.width - 50; right.y = 280;
+	
+	--add jump joystick
+	local middle = display.newRect(0,0,160,160)
+	middle.x = G.width / 2; middle.y = 100;
 
 	
 	function left:touch()
@@ -60,28 +64,70 @@ function scene:create( event )
 	end
 	right:addEventListener("touch", right)
 	
+	function middle:touch()
+		G.motiony = -G.speed;
+	end
+	middle:addEventListener("touch", middle)
+	--]]
+	function onKeyEvent (event)
+		if event.keyName == "a" then
+			if event.phase == "down" then
+				transition.to(xap, {time = 3000, x = xap.x - (display.actualContentWidth / 2)})
+				print ("a key pressed")
+			elseif event.phase == "up" then
+				print ("a key released")
+				transition.cancel()
+			end
+		end
+		if event.keyName == "d" then
+			if event.phase == "down" then
+				transition.to(xap, {time = 3000, x = xap.x + (display.actualContentWidth / 2)})
+				print ("d key pressed")
+			elseif event.phase == "up" then
+				transition.cancel()
+				print ("d key pressed")
+			end
+		end
+		if event.keyName == "w" then
+			if event.phase == "down" then
+				transition.to(xap, {time = 3000, y = xap.y - (display.actualContentHeight / 2)})
+				print ("w key pressed")
+			elseif event.phase == "up" then
+				transition.cancel()
+				print ("w key pressed")
+			end
+		end
+		if event.keyName == "s" then
+			if event.phase == "down" then
+				transition.to(xap, {time = 3000, y = xap.y + (display.actualContentHeight / 2)})
+				print ("s key pressed")
+			elseif event.phase == "up" then
+				transition.cancel()
+				print ("s key pressed")
+			end
+		end
+	end
+	
+	
+	Runtime:addEventListener("key", onKeyEvent)
+	
 	
 	--move character
 	local function movePlayer (event)
 		xap.x = xap.x + G.motionx;
+		xap.y = xap.y + G.motiony;
 	end
 	Runtime:addEventListener("enterFrame", movePlayer)
 	
 	local function stop (event)
   if event.phase =="ended" then
    G.motionx = 0;
+   G.motiony = 0;
   end
  end
  Runtime:addEventListener("touch", stop )
 	
 	
-	--let character jump
-	local function onTouch(event)
-		if(event.phase == "began") then
-		xap:setLinearVelocity(0, -200)
-		end
-	end
-	Runtime:addEventListener("touch", onTouch)
 
 	
 	
