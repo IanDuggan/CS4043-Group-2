33821aaf1ce018c03b774229b8a672ef8232f6ba
MrSwayne <MrSwayne@users.noreply.github.com>
2017-03-15 14:03:58 +0000

    Update globals.lua

--- globals.lua
+++ globals.lua
@@ -4,9 +4,6 @@ G = {}
 G.width = display.actualContentWidth
 G.height = display.actualContentHeight
 
---Xap--
-G.motionx = 0 -- keep this 0 if you want your man to stay still
-G.speed = 2 -- the speed of mr xap
 
 
 --paths--
