9f9a35e90908adb24737d831f75189fcbb2ed334
MrSwayne <MrSwayne@users.noreply.github.com>
2017-03-15 15:45:47 +0000

    Add files via upload

--- levels/level1.lua
+++ levels/level1.lua
@@ -7,7 +7,7 @@ local combat = require ("combat")
 
 
 --Check hitboxes and shit
-physics.setDrawMode("hybrid")
+--physics.setDrawMode("hybrid")
 
 --------------------------------------------
 
@@ -17,33 +17,39 @@ local screenW, screenH, halfW = G.width, G.height, display.contentCenterX
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
-	-- 
+	--
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
 	local sceneGroup = self.view
-	
+
 	physics.start()
 	physics.pause()
 
 
 	-- create a grey rectangle as the backdrop <change as needed>
 	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
-	background.anchorX = 0 
+	background.anchorX = 0
 	background.anchorY = 0
 	background:setFillColor( .5 )
-	
+
 	--adds a floor (no shit)
 	local floor = display.newRect(0, 0, G.width, 40 )
 	floor.x = G.width / 2; floor.y = G.height - 35
 	physics.addBody(floor, "static", { friction = .5, bounce = .3} )
 
 	--adds xap to the game
-	
+
 	xap = display.newImageRect( "capnXap.png", 250, 250 )
 	physics.addBody( xap, "dynamic", {friction = .5, bounce = 0})
 	xap.myName="xap"
 	xap.x = G.width / 2; xap.y = G.height - 180
+
+
+	trap = display.newImageRect("trap.png", 100,100)
+	physics.addBody(trap, "static", {friction = .5, bounce = 0})
+	trap.myName = "trap"
+	trap.x = G.width / 2 - 270; trap.y = G.height - 50
 
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
@@ -55,12 +61,12 @@ end
 function scene:show( event )
 	local sceneGroup = self.view
 	local phase = event.phase
-	
+
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
 		-- Called when the scene is now on screen
-		-- 
+		--
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
 		physics.start()
@@ -69,9 +75,9 @@ end
 
 function scene:hide( event )
 	local sceneGroup = self.view
-	
+
 	local phase = event.phase
-	
+
 	if event.phase == "will" then
 		-- Called when the scene is on screen and is about to move off screen
 		--
@@ -80,18 +86,18 @@ function scene:hide( event )
 		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
-	end	
-	
+	end
+
 end
 
 function scene:destroy( event )
 
 	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
+	--
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
 	local sceneGroup = self.view
-	
+
 	package.loaded[physics] = nil
 	physics = nil
 end
